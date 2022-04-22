61ae67ee6e4890ffadb47c09ce6628c063fc3ea2
welling6570 <35569254+welling6570@users.noreply.github.com>
2022-04-22 13:20:06 -0400

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022ilpe",
+        "defaultValue":"2022mimil",
         "required":"true",
         "disabled":"true"
       },

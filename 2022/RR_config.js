8ecda0897bc32527ff5ed1153a87b4787af5d3e4
007sharko <103892433+007sharko@users.noreply.github.com>
2022-04-24 14:32:31 -0400

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022mimil",
+        "defaultValue":"2022misal",
         "required":"true",
         "disabled":"true"
       },
